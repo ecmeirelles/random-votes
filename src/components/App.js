fb6346e8d91c6d774417d4de4b00c507468d224f
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2018-11-19 21:54:55 -0200

    List questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
+import moment from "moment";
+import { Grid, Card } from "semantic-ui-react";
+import fetchQuestions from "../services/fetchQuestions";
 
 class App extends Component {
+  state = {
+    questions: null
+  };
+
+  componentDidMount() {
+    fetchQuestions().then((questions) => {
+      this.setState({ questions });
+    });
+  };
+
   render() {
+    const { questions } = this.state;
+    const numberChoices = (question) => `${ question.choices.length } choices`;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ padding: "1em" }}>
+        <h1 style={{ textAlign: "center", marginTop: "1em", marginBottom: "2em" }}>
+          Questions
+        </h1>
+        <Grid doubling columns={4}>
+          { questions && questions.map(question => (
+          <Grid.Column key={ Math.random() }>
+            <Card
+              href="#card-example-link-card"
+              header={ question.question }
+              meta={ moment(question.published_at).format("MM-DD-YYYY") }
+              description={ numberChoices(question) }
+            />
+          </Grid.Column>
+          ))}
+        </Grid>
       </div>
     );
   }
